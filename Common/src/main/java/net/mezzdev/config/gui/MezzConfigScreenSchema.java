d4b53bb4c60c02d85982723844e3e7a04fc82cd8
mezz <tehgeek@gmail.com>
2026-09-23 00:16:59 +0900

    Remove unused code
    (cherry picked from commit 0f8e834bf4bc602cc2c50d352643106976b94e5b)

--- Common/src/main/java/net/mezzdev/config/gui/MezzConfigScreenSchema.java
+++ Common/src/main/java/net/mezzdev/config/gui/MezzConfigScreenSchema.java
@@ -37,7 +37,7 @@ final class MezzConfigScreenSchema implements ConfigScreenSchema {
 
 	@Override
 	public List<? extends ConfigScreenCategory> getCategories() {
-		return createCategories(schema, schema.getCategories(), schema.getEditorCategories());
+		return createCategories(schema.getCategories(), schema.getEditorCategories());
 	}
 
 	@Override
@@ -50,7 +50,6 @@ final class MezzConfigScreenSchema implements ConfigScreenSchema {
 	}
 
 	private static List<ConfigScreenCategory> createCategories(
-		IConfigSchema schema,
 		List<? extends IConfigCategory> categories,
 		List<? extends IConfigEditorCategory> editorCategories
 	) {
--- Common/src/main/java/net/mezzdev/config/gui/entries/IntegerConfigEntry.java
+++ Common/src/main/java/net/mezzdev/config/gui/entries/IntegerConfigEntry.java
@@ -5,7 +5,6 @@ import net.mezzdev.config.gui.ConfigInputUtil;
 import net.mezzdev.config.gui.util.ConfigMath;
 
 import net.mezzdev.config.api.value.serializer.ConfigValueRange;
-import net.mezzdev.config.api.value.serializer.IConfigValueSerializer;
 import net.mezzdev.config.gui.ConfigGuiColors;
 import net.mezzdev.config.gui.api.ConfigInfo;
 import net.mezzdev.config.gui.api.ConfigValueLocalization;
@@ -40,7 +39,6 @@ final class IntegerConfigEntry extends ConfigEntryWidget<Integer> {
 	private static final int NORMAL_STEP = 1;
 	private static final int SHIFT_STEP = 10;
 
-	private final IConfigValueSerializer<Integer> serializer;
 	private final int min;
 	private final int max;
 	private ImmutableRect2i valueBoxArea = ImmutableRect2i.EMPTY;
@@ -52,12 +50,10 @@ final class IntegerConfigEntry extends ConfigEntryWidget<Integer> {
 
 	IntegerConfigEntry(
 		IConfigScreenValue<Integer> value,
-		IConfigValueSerializer<Integer> serializer,
 		ConfigValueRange<Integer> range,
 		ConfigTextures textures
 	) {
 		super(value, textures);
-		this.serializer = serializer;
 		this.min = range.min();
 		this.max = range.max();
 	}
