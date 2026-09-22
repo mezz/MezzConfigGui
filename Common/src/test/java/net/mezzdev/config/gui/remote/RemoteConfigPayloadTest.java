6c2dc81893b1d2b537a507c0cb856567ab28bf2d
mezz <tehgeek@gmail.com>
2026-09-23 01:00:37 +0900

    Complete NullMarked package coverage

--- Common/src/test/java/net/mezzdev/config/gui/remote/RemoteConfigPayloadTest.java
+++ Common/src/test/java/net/mezzdev/config/gui/remote/RemoteConfigPayloadTest.java
@@ -1,5 +1,6 @@
 package net.mezzdev.config.gui.remote;
 
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -89,6 +90,7 @@ class RemoteConfigPayloadTest {
 		List<byte[]> chunks = new ArrayList<>(RemoteConfigPayloadChunker.split(data, 10));
 		Collections.reverse(chunks);
 		RemoteConfigPayloadReassembler reassembler = new RemoteConfigPayloadReassembler();
+		@Nullable
 		byte[] result = null;
 		for (byte[] chunk : chunks) {
 			result = reassembler.accept(chunk, 0).orElse(result);
